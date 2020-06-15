544a3fc9f6e09def268b2f541822bd8ccbeeccc6
fkaraagac11 <fehmi11.qa@gmail.com>
2020-06-15 19:52:23 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,26 @@
+const btns = document.querySelectorAll(".btn");
+const value = document.querySelector("#value");
+
+let count = 0;
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            count--;
+        } else if (styles.contains("increase")) {
+            count++;
+        } else {
+            count = 0;
+        }
+        value.textContent = count;
+
+        if (count < 0) {
+            value.style.color = "red";
+        } else if (count > 0) {
+            value.style.color = "blue";
+        } else {
+            value.style.color = "black";
+        }
+    });
+});
